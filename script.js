c99083adfbba654617c52d02bc0731a9d165b8b7
Ritik Yadav <85021787+idk-Ritikyadav@users.noreply.github.com>
2022-09-29 21:54:29 +0545

    Update script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function notice() {
+    document.getElementById("gamma").style.display = "none";
+    document.getElementById("beta").style.display = "none";
+    document.getElementById("alpha").style.display = "";
+    document.getElementById("alpha").style.width = "100%";
+
+}
+
+
+
+function home() {
+    document.getElementById("gamma").style.display = "none";
+    document.getElementById("alpha").style.display = "none";
+    document.getElementById("beta").style.display = "";
+    document.getElementById("beta").style.width = "100%";
+}
+
+
+
+function chat() {
+    document.getElementById("alpha").style.display = "none";
+    document.getElementById("beta").style.display = "none";
+    document.getElementById("gamma").style.display = "";
+    document.getElementById("gamma").style.width = "100%";
+
+
+}
